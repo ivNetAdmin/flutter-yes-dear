d5e31db9efbb84609de165f4f13e92753c5dfd67
ivNetAdmin <bp@ivnet.co.uk>
2025-01-06 14:38:08 +0000

    Added task list to home screen

--- lib/data/repositories/task/task_repository_local.dart
+++ lib/data/repositories/task/task_repository_local.dart
@@ -2,12 +2,58 @@ import '../../../domain/models/task/task_summary.dart';
 import '../../../utils/result.dart';
 import 'task_repository.dart';
 
-class TaskRepositoryLocal implements TaskRepository
-{
-  @override
-  Future<Result<List<TaskSummary>>> getTaskSummaryList() {
+class TaskRepositoryLocal implements TaskRepository {
 
-    throw UnimplementedError();
+  bool _isInitialized = false;
+  int _sequentialId = 0;
+
+  final _tasks = List<TaskSummary>.empty(growable: true);
+
+  @override
+  Future<Result<List<TaskSummary>>> getTaskSummaryList() async {
+    // Initialize the repository with a default booking
+    if (!_isInitialized) {
+      await _createDefaultTask();
+      _isInitialized = true;
+    }
+    return Result.ok(_createSummaries());
   }
 
+  List<TaskSummary> _createSummaries() {
+    return _tasks
+        .map(
+          (task) =>
+          TaskSummary(
+              id: task.id!,
+              description: task.description,
+              createDate: task.createDate
+          ),
+    ).toList();
+  }
+
+  Future<void> _createDefaultTask() async {
+    if (_tasks.isEmpty) {
+      _tasks.add(
+        TaskSummary(
+          id: _sequentialId++,
+          description: 'My first task!',
+          createDate: DateTime.now(), //  (2025, 1, 6),
+        ),
+      );
+
+      _tasks.add(
+        TaskSummary(
+          id: _sequentialId++,
+          description: 'My next task!',
+          createDate: DateTime.now(), //  (2025, 1, 6),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Result<void>> delete(int id) async {
+    _tasks.removeWhere((task) => task.id == id);
+    return const Result.ok(null);
+  }
 }
--- lib/ui/home_feature/widgets/home_screen.dart
+++ lib/ui/home_feature/widgets/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../domain/models/task/task_summary.dart';
+import '../../core/themes/colours.dart';
+import '../../core/themes/dimens.dart';
 import '../../core/ui/app_bar_widget.dart';
 import '../../core/ui/bottom_navigation_bar_widget.dart';
 import '../view_model/home_viewmodel.dart';
@@ -14,14 +17,128 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => context.go('/details'),
-          child: const Text('Go to the Details screen'),
+      body: SafeArea(
+        child: ListenableBuilder(
+            listenable: viewModel,
+            builder: (context, _) {
+              return CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: Dimens
+                            .of(context)
+                            .paddingScreenVertical,
+                        horizontal: Dimens
+                            .of(context)
+                            .paddingScreenHorizontal,
+                      ),
+                      child: Text(
+                          'Yes Dear!'), //HomeHeader(viewModel: widget.viewModel),
+                    ),
+                  ),
+                  SliverList.builder(
+                    itemCount: viewModel.tasks.length,
+                    itemBuilder: (_, index) =>
+                        _TaskSummary(
+                            key: ValueKey(viewModel.tasks[index].id),
+                            task: viewModel.tasks[index],
+                          confirmDismiss: (_) async {
+                            // wait for command to complete
+                            await viewModel.deleteTask.execute(
+                              checkNull(viewModel.tasks[index].id),
+                            );
+                            // if command completed successfully, return true
+                            if (viewModel.deleteTask.completed) {
+                              // removes the dismissible from the list
+                              return true;
+                            } else {
+                              // the dismissible stays in the list
+                              return false;
+                            }
+                          },
+                        ),
+                  ),
+                ],
+              );
+            }
         ),
       ),
-      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  int checkNull(int? id) {
+    if (id == null) {
+      return 0;
+    } else {
+      return id;
+    }
+  }
+}
+
+class _TaskSummary extends StatelessWidget {
+  const _TaskSummary({
+    super.key,
+    required this.task,
+    required this.confirmDismiss,
+  });
+
+  final TaskSummary task;
+  final ConfirmDismissCallback confirmDismiss;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(task.id),
+      direction: DismissDirection.endToStart,
+      onDismissed: confirmDismiss,
+      background: Container(
+        color: AppColors.grey1,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: Dimens.paddingHorizontal),
+              child: Icon(Icons.delete),
+            ),
+          ],
+        ),
+      ),
+      child: InkWell(
+        //onTap: onTap,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Dimens
+                .of(context)
+                .paddingScreenHorizontal,
+            vertical: Dimens.paddingVertical,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                task.description,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .titleLarge,
+              ),
+              Text(
+                //dateFormatStartEnd(
+                //  DateTimeRange(
+                //    start: booking.startDate,
+                //    end: booking.endDate,
+                //  ),
+                task.createDate.toString(),
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .bodyLarge,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
